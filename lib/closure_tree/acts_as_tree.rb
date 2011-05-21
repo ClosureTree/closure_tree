c1b875e5fd189e8965b79b3ee16f3616873feafb
Matthew McEachen <matthew+github@mceachen.org>
2011-05-21 12:16:46 -0700

    checkpoint

--- lib/closure_tree/acts_as_tree.rb
+++ lib/closure_tree/acts_as_tree.rb
@@ -1,15 +1,37 @@
 module ClosureTree #:nodoc:
   module ActsAsTree #:nodoc:
-          def acts_as_tree(options = {})
-        options = {
-          :parent_column => 'parent_id',
-          :dependent => :delete_all, # or :destroy
-          :heirarchy_table_suffix => '_heirarchy'
-        }.merge(options)
+    def acts_as_tree(options = {})
 
-        if options[:scope].is_a?(Symbol) && options[:scope].to_s !~ /_id$/
-          options[:scope] = "#{options[:scope]}_id".intern
+      write_inheritable_attribute :closure_tree_options, {
+        :parent_column_name => 'parent_id',
+        :dependent => :delete_all, # or :destroy
+        :hierarchy_table_name => '_hierarchy'
+      }.merge(options)
+
+      class_inheritable_reader :closure_tree_options
+              include Columns
+        extend Columns
+
+      belongs_to :parent, :class_name => self.base_class.to_s,
+          :foreign_key => parent_column_name,
+          :counter_cache => options[:counter_cache]
+
+    end
+  end
+
+
+      # Mixed into both classes and instances to provide easy access to the column names
+      module Columns
+        def parent_column_name
+          acts_as_nested_set_options[:parent_column]
         end
 
-  end
+        def scope_column_names
+          Array(acts_as_nested_set_options[:scope])
+        end
+
+        def quoted_parent_column_name
+          connection.quote_column_name(parent_column_name)
+        end
+      end
 end
