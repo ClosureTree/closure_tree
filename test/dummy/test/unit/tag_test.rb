a43b822765dac5d5257243c8131e98524e4f82bf
Matthew McEachen <matthew+github@mceachen.org>
2011-05-24 12:21:21 -0700

    added .children()

--- test/dummy/test/unit/tag_test.rb
+++ test/dummy/test/unit/tag_test.rb
@@ -20,17 +20,32 @@ class TagTest < ActiveSupport::TestCase
   def test_add_child
     sb = Tag.create!(:name => "Santa Barbara")
     assert sb.leaf?
-    tags(:california).add_child sb
+    ca = tags(:california)
+    ca.add_child sb
     sb.reload
     assert sb.leaf?
     sb_ancestors = sb.ancestor_ids_with_generations
-    assert_equal [[tags(:places).id, 3], [tags(:united_states).id, 2], [tags(:california).id, 1], [sb.id, 0]], sb_ancestors
+    assert_equal [[tags(:places).id, 3], [tags(:united_states).id, 2], [ca.id, 1], [sb.id, 0]], sb_ancestors
+
+    assert ca.children.include?(sb)
   end
 
   def test_level
     assert_equal 0, tags(:grandparent).level
     assert_equal 1, tags(:parent).level
     assert_equal 2, tags(:child).level
+  end
+
+  def test_parent
+    assert_equal nil, tags(:grandparent).parent
+    assert_equal tags(:grandparent), tags(:parent).parent
+    assert_equal tags(:parent), tags(:child).parent
+  end
+
+  def test_children
+    assert tags(:grandparent).children.include? tags(:parent)
+    assert tags(:parent).children.include? tags(:child)
+    assert tags(:child).children.empty?
   end
 
 end
