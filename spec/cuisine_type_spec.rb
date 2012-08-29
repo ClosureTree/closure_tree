a13ed9bce7aeeed7d569fd85d6a83943142c6edb
Matthew McEachen <matthew+github@mceachen.org>
2012-08-28 21:31:32 -0700

    test for lineage when constructors are used instead of .children <<

--- spec/cuisine_type_spec.rb
+++ spec/cuisine_type_spec.rb
@@ -1,18 +1,27 @@
 require 'spec_helper'
 
+def assert_lineage(e, m)
+  m.parent.should == e
+  m.self_and_ancestors.should == [m, e]
+
+  # make sure reloading doesn't affect the self_and_ancestors:
+  m.reload
+  m.self_and_ancestors.should == [m, e]
+end
+
 describe CuisineType do
-  it "finds self and parents properly" do
+  it "finds self and parents when children << is used" do
     e = CuisineType.new(:name => "e")
     m = CuisineType.new(:name => "m")
     e.children << m
     e.save
+    assert_lineage(e, m)
+  end
 
-    m.parent.should == e
-    m.self_and_ancestors.should == [m, e]
-
-    # make sure reloading doesn't affect the self_and_ancestors:
-    m.reload
-    m.self_and_ancestors.should == [m, e]
+  it "finds self and parents properly if the constructor is used" do
+    e = CuisineType.create(:name => "e")
+    m = CuisineType.create(:name => "m", :parent => e)
+    assert_lineage(e, m)
   end
 
   it "sets the table_name of the hierarchy class properly" do
