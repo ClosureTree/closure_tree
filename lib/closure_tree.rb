5288a11391e5eb7927b0b4184701b25af84c8655
Matthew McEachen <matthew+github@mceachen.org>
2013-02-18 19:25:03 -0800

    get the requires in the right order

--- lib/closure_tree.rb
+++ lib/closure_tree.rb
@@ -2,8 +2,8 @@ require 'active_support'
 require 'active_record'
 
 ActiveSupport.on_load :active_record do
-  require 'closure_tree/acts_as_tree'
   require 'with_advisory_lock'
+  require 'closure_tree/acts_as_tree'
 
   ActiveRecord::Base.send :extend, ClosureTree::ActsAsTree
 end
