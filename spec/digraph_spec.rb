1879f55e98f6cc115fac62c83f757cf31436a934
Matthew McEachen <matthew+github@mceachen.org>
2013-06-01 14:03:41 -0700

    stable IDs

--- spec/digraph_spec.rb
+++ spec/digraph_spec.rb
@@ -8,20 +8,21 @@ describe 'DOT rendering' do
     Tag.find_or_create_by_path(%w(a b1 c1))
     Tag.find_or_create_by_path(%w(a b2 c2))
     Tag.find_or_create_by_path(%w(a b2 c3))
+    a, b1, b2, c1, c2, c3 = %w(a b1 b2 c1 c2 c3).map { |ea| Tag.where(:name => ea).first.id }
     dot = Tag.roots.first.to_dot_digraph
     dot.should == <<-DOT
 digraph G {
-  1 [label="a"]
-  1 -> 2
-  2 [label="b1"]
-  1 -> 4
-  4 [label="b2"]
-  2 -> 3
-  3 [label="c1"]
-  4 -> 5
-  5 [label="c2"]
-  4 -> 6
-  6 [label="c3"]
+  #{a} [label="a"]
+  #{a} -> #{b1}
+  #{b1} [label="b1"]
+  #{a} -> #{b2}
+  #{b2} [label="b2"]
+  #{b1} -> #{c1}
+  #{c1} [label="c1"]
+  #{b2} -> #{c2}
+  #{c2} [label="c2"]
+  #{b2} -> #{c3}
+  #{c3} [label="c3"]
 }
     DOT
   end
