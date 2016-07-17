bb0e6e19434b56d65c207718a402c722397ca544
Abdelkader Boudih <terminale@gmail.com>
2016-07-17 18:49:34 +0100

    avoid breaking change for Heroku

--- lib/closure_tree/configuration.rb
+++ lib/closure_tree/configuration.rb
@@ -3,7 +3,7 @@ module ClosureTree
     attr_accessor :database_less
 
     def initialize
-      @database_less = false
+      @database_less = ENV['DATABASE_URL'].to_s.include?('//user:pass@127.0.0.1/')
     end
   end
 end
