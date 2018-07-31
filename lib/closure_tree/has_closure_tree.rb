7ee5ab3d0a87b09733b3e51205736bd6e662a916
Joey Marianer <rmarianer@instructure.com>
2018-07-31 15:50:01 -0700

    Use with_connection instead of calling release_connection directly, since the latter may release a connection that belongs to someone else

--- lib/closure_tree/has_closure_tree.rb
+++ lib/closure_tree/has_closure_tree.rb
@@ -14,25 +14,25 @@ module ClosureTree
         :with_advisory_lock
       )
 
-      class_attribute :_ct
-      self._ct = ClosureTree::Support.new(self, options)
+      connection_pool.with_connection {
+        class_attribute :_ct
+        self._ct = ClosureTree::Support.new(self, options)
 
-      # Auto-inject the hierarchy table
-      # See https://github.com/patshaughnessy/class_factory/blob/master/lib/class_factory/class_factory.rb
-      class_attribute :hierarchy_class
-      self.hierarchy_class = _ct.hierarchy_class_for_model
+        # Auto-inject the hierarchy table
+        # See https://github.com/patshaughnessy/class_factory/blob/master/lib/class_factory/class_factory.rb
+        class_attribute :hierarchy_class
+        self.hierarchy_class = _ct.hierarchy_class_for_model
 
-      # tests fail if you include Model before HierarchyMaintenance wtf
-      include ClosureTree::HierarchyMaintenance
-      include ClosureTree::Model
-      include ClosureTree::Finders
-      include ClosureTree::HashTree
-      include ClosureTree::Digraphs
+        # tests fail if you include Model before HierarchyMaintenance wtf
+        include ClosureTree::HierarchyMaintenance
+        include ClosureTree::Model
+        include ClosureTree::Finders
+        include ClosureTree::HashTree
+        include ClosureTree::Digraphs
 
-      include ClosureTree::DeterministicOrdering if _ct.order_option?
-      include ClosureTree::NumericDeterministicOrdering if _ct.order_is_numeric?
-
-      connection_pool.release_connection
+        include ClosureTree::DeterministicOrdering if _ct.order_option?
+        include ClosureTree::NumericDeterministicOrdering if _ct.order_is_numeric?
+      }
     rescue StandardError => e
       raise e unless ClosureTree.configuration.database_less
     end
