f3b33f81157ccf002b8c2774b30dc35bd4722610
shawndodo <dodocat6666@hotmail.com>
2021-09-17 16:42:18 +0800

    fix: hierarchy model with namespace should inherit from the superclass of basic model (#384)
    Co-authored-by: xiaoyu <shawn.zhu@chowbus.com>

--- lib/closure_tree/support.rb
+++ lib/closure_tree/support.rb
@@ -33,7 +33,7 @@ module ClosureTree
 
     def hierarchy_class_for_model
       parent_class = ActiveSupport::VERSION::MAJOR >= 6 ? model_class.module_parent : model_class.parent
-      hierarchy_class = parent_class.const_set(short_hierarchy_class_name, Class.new(ActiveRecord::Base))
+      hierarchy_class = parent_class.const_set(short_hierarchy_class_name, Class.new(model_class.superclass))
       use_attr_accessible = use_attr_accessible?
       include_forbidden_attributes_protection = include_forbidden_attributes_protection?
       model_class_name = model_class.to_s
