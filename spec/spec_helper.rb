fdfcd612f521f6a6506d138f34358d7f00476910
Abdelkader Boudih <terminale@gmail.com>
2022-07-31 10:53:15 +0100

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,14 +11,36 @@ require 'parallel'
 require 'active_record'
 require 'active_support/core_ext/array'
 
-# Use in specs to skip some tests
-def sqlite?
-  ENV.fetch('DB_ADAPTER', 'sqlite3') == 'sqlite3'
-end
 
 # Start Simplecov
 SimpleCov.start do
   add_filter 'spec/'
+end
+
+ActiveRecord::Base.configurations = {
+  default_env: {
+    url: ENV.fetch('DATABASE_URL', "sqlite3::memory:"),
+    properties: { allowPublicKeyRetrieval: true } # for JRuby madness
+  }
+}
+
+# Configure ActiveRecord
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Base.table_name_prefix = ENV['DB_PREFIX'].to_s
+ActiveRecord::Base.table_name_suffix = ENV['DB_SUFFIX'].to_s
+ActiveRecord::Base.establish_connection
+
+def env_db
+  @env_db ||= if ActiveRecord::Base.respond_to?(:connection_db_config)
+                ActiveRecord::Base.connection_db_config.adapter
+              else
+                ActiveRecord::Base.connection_config[:adapter]
+              end.to_sym
+end
+
+# Use in specs to skip some tests
+def sqlite?
+  env_db == :sqlite3
 end
 
 # Configure RSpec
@@ -29,7 +51,6 @@ RSpec.configure do |config|
   config.fail_fast = false
 
   config.order = :random
-  Kernel.srand config.seed
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
@@ -68,27 +89,7 @@ Thread.abort_on_exception = true
 # See: https://github.com/ClosureTree/with_advisory_lock
 ENV['WITH_ADVISORY_LOCK_PREFIX'] ||= SecureRandom.hex
 
-# Configure ActiveRecord
-ActiveRecord::Migration.verbose = false
-ActiveRecord::Base.table_name_prefix = ENV['DB_PREFIX'].to_s
-ActiveRecord::Base.table_name_suffix = ENV['DB_SUFFIX'].to_s
-
-ActiveRecord::Base.configurations = {
-  default_env: {
-    url: ENV.fetch('DATABASE_URL', "sqlite3::memory:"),
-    properties: { allowPublicKeyRetrieval: true } # for JRuby madness
-  }
-}
-def env_db
-  @env_db ||= if ActiveRecord::Base.respond_to?(:connection_db_config)
-                ActiveRecord::Base.connection_db_config.adapter
-              else
-                ActiveRecord::Base.connection_config[:adapter]
-              end.to_sym
-end
-
-ActiveRecord::Base.establish_connection
-ActiveRecord::Base.connection.recreate_database("closure_tree_test")
+ActiveRecord::Base.connection.recreate_database("closure_tree_test") unless sqlite?
 puts "Testing with #{env_db} database, ActiveRecord #{ActiveRecord.gem_version} and #{RUBY_ENGINE} #{RUBY_ENGINE_VERSION} as #{RUBY_VERSION}"
 # Require our gem
 require 'closure_tree'
