f3a8f08919954b69957b5327a4d1fb87f8a01506
Albert Agram <albert.agram@gmail.com>
2022-08-06 14:58:15 -0400

    adds support test

---
+++ test/closure_tree/support_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+describe ClosureTree::Support do
+  let(:sut) { Tag._ct }
+
+  it 'passes through table names without prefix and suffix' do
+    expected = 'some_random_table_name'
+    assert_equal expected, sut.remove_prefix_and_suffix(expected)
+  end
+
+  it 'extracts through table name with prefix and suffix' do
+    expected = 'some_random_table_name'
+    tn = ActiveRecord::Base.table_name_prefix + expected + ActiveRecord::Base.table_name_suffix
+    assert_equal expected, sut.remove_prefix_and_suffix(tn)
+  end
+end
