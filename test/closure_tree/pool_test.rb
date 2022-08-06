d0c3e4e89dffa6d8e216660a6323c13f5c2448c9
Albert Agram <albert.agram@gmail.com>
2022-08-06 14:57:59 -0400

    adds pool test

---
+++ test/closure_tree/pool_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+describe "Configuration" do
+  it 'returns connection to the pool after has_closure_tree setup' do
+    class TypeDuplicate < ActiveRecord::Base
+      self.table_name = "namespace_type#{table_name_suffix}"
+      has_closure_tree
+    end
+
+    refute ActiveRecord::Base.connection_pool.active_connection?
+    # +false+ in AR 4, +nil+ in AR 5
+  end
+
+  it 'returns connection to the pool after has_closure_tree setup with order' do
+    class MetalDuplicate < ActiveRecord::Base
+      self.table_name = "#{table_name_prefix}metal#{table_name_suffix}"
+      has_closure_tree order: 'sort_order', name_column: 'value'
+    end
+
+    refute ActiveRecord::Base.connection_pool.active_connection?
+  end
+
+  it 'returns connection to the pool after has_closure_tree_root setup' do
+    class GroupDuplicate < ActiveRecord::Base
+      self.table_name = "#{table_name_prefix}group#{table_name_suffix}"
+      has_closure_tree_root :root_user
+    end
+
+    refute ActiveRecord::Base.connection_pool.active_connection?
+  end
+end
