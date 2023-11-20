a0023845eaf4c84c47a9bdecdd71dfa59b9b25b0
Jens Kraemer <jk@jkraemer.net>
2023-11-20 16:48:18 +0800

    use force: :cascade so tables can be recreated regardless of constraints

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveRecord::Schema.define(version: 0) do
-  create_table 'tags', force: true do |t|
+  create_table 'tags', force: :cascade do |t|
     t.string 'name'
     t.string 'title'
     t.references 'parent'
@@ -9,13 +9,13 @@ ActiveRecord::Schema.define(version: 0) do
     t.timestamps null: false
   end
 
-  create_table 'tag_hierarchies', id: false, force: true do |t|
+  create_table 'tag_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'uuid_tags', id: false, force: true do |t|
+  create_table 'uuid_tags', id: false, force: :cascade do |t|
     t.string 'uuid', unique: true
     t.string 'name'
     t.string 'title'
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 0) do
     t.timestamps null: false
   end
 
-  create_table 'uuid_tag_hierarchies', id: false, force: true do |t|
+  create_table 'uuid_tag_hierarchies', id: false, force: :cascade do |t|
     t.string 'ancestor_id', null: false
     t.string 'descendant_id', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'destroyed_tags', force: true do |t|
+  create_table 'destroyed_tags', force: :cascade do |t|
     t.string 'name'
   end
 
@@ -38,81 +38,81 @@ ActiveRecord::Schema.define(version: 0) do
                                                                           name: 'tag_anc_desc_idx'
   add_index 'tag_hierarchies', [:descendant_id], name: 'tag_desc_idx'
 
-  create_table 'groups', force: true do |t|
+  create_table 'groups', force: :cascade do |t|
     t.string 'name', null: false
   end
 
-  create_table 'groupings', force: true do |t|
+  create_table 'groupings', force: :cascade do |t|
     t.string 'name', null: false
   end
 
-  create_table 'user_sets', force: true do |t|
+  create_table 'user_sets', force: :cascade do |t|
     t.string 'name', null: false
   end
 
-  create_table 'teams', force: true do |t|
+  create_table 'teams', force: :cascade do |t|
     t.string 'name', null: false
   end
 
-  create_table 'users', force: true do |t|
+  create_table 'users', force: :cascade do |t|
     t.string 'email'
     t.references 'referrer'
     t.integer 'group_id'
     t.timestamps null: false
   end
 
-  create_table 'contracts', force: true do |t|
+  create_table 'contracts', force: :cascade do |t|
     t.references 'user', null: false
     t.references 'contract_type'
     t.string 'title'
   end
 
-  create_table 'contract_types', force: true do |t|
+  create_table 'contract_types', force: :cascade do |t|
     t.string 'name', null: false
   end
 
-  create_table 'referral_hierarchies', id: false, force: true do |t|
+  create_table 'referral_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'labels', force: true do |t|
+  create_table 'labels', force: :cascade do |t|
     t.string 'name'
     t.string 'type'
     t.integer 'column_whereby_ordering_is_inferred'
     t.references 'mother'
   end
 
-  create_table 'label_hierarchies', id: false do |t|
+  create_table 'label_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'cuisine_types', force: true do |t|
+  create_table 'cuisine_types', force: :cascade do |t|
     t.string 'name'
     t.references 'parent'
   end
 
-  create_table 'cuisine_type_hierarchies', id: false, force: true do |t|
+  create_table 'cuisine_type_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'namespace_types', force: true do |t|
+  create_table 'namespace_types', force: :cascade do |t|
     t.string 'name'
     t.references 'parent'
   end
 
-  create_table 'namespace_type_hierarchies', id: false, force: true do |t|
+  create_table 'namespace_type_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'metal', force: true do |t|
+  create_table 'metal', force: :cascade do |t|
     t.references 'parent'
     t.string 'metal_type'
     t.string 'value'
@@ -120,19 +120,19 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer 'sort_order'
   end
 
-  create_table 'metal_hierarchies', id: false, force: true do |t|
+  create_table 'metal_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
   end
 
-  create_table 'menu_items', force: true do |t|
+  create_table 'menu_items', force: :cascade do |t|
     t.string 'name'
     t.references 'parent'
     t.timestamps null: false
   end
 
-  create_table 'menu_item_hierarchies', id: false, force: true do |t|
+  create_table 'menu_item_hierarchies', id: false, force: :cascade do |t|
     t.references 'ancestor', null: false
     t.references 'descendant', null: false
     t.integer 'generations', null: false
